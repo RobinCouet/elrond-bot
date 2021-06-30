a5bedd5f4a7ecea8bb4571c2f040d4875d834129
Robin Couet <couetrobin@gmail.com>
2021-06-30 15:57:36 +0200

    Use embed message

--- commands/prix.js
+++ commands/prix.js
@@ -1,8 +1,21 @@
 const axios = require('axios');
+const Discord = require('discord.js');
+
 module.exports = {
     run: function(message) {
-        axios.get('https://api.coingecko.com/api/v3/simple/price?ids=elrond-erd-2&vs_currencies=USD').then((result) => {
-            message.channel.send('Le prix de l\'EGLD est de : ' + result.data['elrond-erd-2'].usd + ' $');
+        axios.get('https://api.coingecko.com/api/v3/simple/price?ids=elrond-erd-2&vs_currencies=USD,EUR').then((result) => {
+
+            const embed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setURL('https://www.coingecko.com/en/coins/elrond')
+                .setAuthor('Elrond-Bot', 'https://cryptologos.cc/logos/elrond-egld-egld-logo.png?v=012', 'https://github.com/RobinCouet/elrond-bot')
+                .setDescription('Le prix de l\'EGLD est de : ')
+                .addFields(
+                    { name: '$ USD', value: result.data['elrond-erd-2'].usd + ' $' },
+                    { name: '€ EUR', value: result.data['elrond-erd-2'].eur + ' €' },
+                )
+                .setTimestamp();
+            message.channel.send(embed);
         });
     },
     name: 'prix'
